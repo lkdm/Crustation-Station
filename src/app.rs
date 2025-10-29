a60e8fe0eb5a93d21a8a8d908dfa04188969f244
Luke M <git@lukm.dev>
2025-10-30 08:49:44 +1100

    chore: create basic app-wide state.

--- src/app.rs
+++ src/app.rs
@@ -5,8 +5,23 @@ mod list;
 use crate::app::{detail::DetailView, layout::Layout, list::ListView};
 use leptos::prelude::*;
 
+/// TODO: Implement IndexedDB storage for AppState
+pub struct AppState {
+    pub dark_mode: RwSignal<bool>,
+}
+
+impl AppState {
+    fn new() -> Self {
+        Self {
+            dark_mode: RwSignal::new(false),
+        }
+    }
+}
+
 #[component]
 pub fn App() -> impl IntoView {
+    let state = AppState::new();
+    provide_context(state);
     view! {
         <Layout
             sidebar=move || view!{<ListView/>}
