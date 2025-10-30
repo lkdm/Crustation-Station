64030722486a2457bafd48b48494e5e0bdc7b766
Luke M <git@lukm.dev>
2025-10-30 14:31:05 +1100

    chore: get copy result working

--- src/features/json.rs
+++ src/features/json.rs
@@ -1,5 +1,6 @@
 use leptos::prelude::*;
 use leptos_meta::Title;
+use leptos_shadcn_button::Button;
 use leptos_shadcn_textarea::Textarea;
 use serde_json::{Value, to_string_pretty};
 
@@ -21,7 +22,7 @@ pub fn JsonParserFormatter() -> impl IntoView {
 
     Effect::new(move |_| {
         let text = input.get();
-        if (!form_touched.get()) {
+        if !form_touched.get() {
             // If user has not touched the form, don't change the result
             return;
         }
@@ -41,7 +42,7 @@ pub fn JsonParserFormatter() -> impl IntoView {
     });
 
     view! {
-        <div class="flex flex-row gap-2 h-full">
+        <div class="flex flex-row gap-4 h-full">
             <Title text="JSON Parser and Formatter"/>
             // Input textarea
             <feature-input class="flex-1 h-full">
@@ -56,8 +57,26 @@ pub fn JsonParserFormatter() -> impl IntoView {
                 />
             </feature-input>
             // Result area
-            <feature-result class="flex-1 h-full border border-border rounded-md overflow-auto p-4 bg-secondary text-secondary-foreground">
-                <pre class="h-full w-full font-mono text-xs">
+            <feature-result class="flex flex-col flex-1 h-full w-full gap-4">
+                <feature-toolbar>
+                    <Button
+                        on_click=Callback::new(move |_| {
+                            match result.get() {
+                                Ok(val) => {
+                                    // Get the window from web_sys
+                                    let window = web_sys::window().unwrap();
+                                    let navigator = window.navigator();
+                                    let clipboard = navigator.clipboard();
+                                    let _ = clipboard.write_text(&val);
+                                }
+                                Err(_) => {}
+                            }
+                        })
+                    >
+                        "Copy"
+                    </Button>
+                </feature-toolbar>
+                <pre class="h-full w-full font-mono text-xs flex-1 border border-border rounded-md overflow-auto p-4 bg-secondary text-secondary-foreground">
                     {move || match result.get() {
                         Ok(val) => val.clone(),
                         Err(err) => err,
