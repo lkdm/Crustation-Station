f6ef9224b921105f7314875d1b9a282e40230d87
Luke M <git@lukm.dev>
2025-10-30 12:10:48 +1100

    chore(ui): create branding

--- src/app/list.rs
+++ src/app/list.rs
@@ -1,15 +1,19 @@
 use leptos::prelude::*;
+use leptos_router::components::A;
 
 #[component]
 pub fn Branding() -> impl IntoView {
     view! {
-        "Crustacean Station"
+        <div class="text-center">
+            <A href="/"><h1 class="font-mono text-xs">"🦀 Crustacean Station"</h1></A>
+        </div>
     }
 }
 
 #[component]
 pub fn ListView() -> impl IntoView {
     view! {
+        <Branding />
         "Sidebar"
     }
 }
