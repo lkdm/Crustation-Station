86d201c3d5e9c03251ba3ee279b4abfa83c4ba10
Luke M <git@lukm.dev>
2025-10-31 15:37:13 +1100

    chore(ui): bold the h1

--- src/app/detail.rs
+++ src/app/detail.rs
@@ -5,7 +5,7 @@ use leptos_meta::*;
 pub fn DetailView(title: String, children: Children) -> impl IntoView {
     view! {
         <>
-            <h1>{title.clone()}</h1>
+            <h1 class="font-semibold">{title.clone()}</h1>
             <Title text=title />
             <div style="height: calc(100vh - 64px)">
                 {children()}
